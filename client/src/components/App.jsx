9beb324e4874b96ff3dc3dc414281bec606426b2
Ingo Speckens <doningo@ymail.com>
2021-01-28 14:59:45 +0100

    dev/create_react_frontend

---
+++ client/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function App() {
+  return (
+    <div>
+      <h1>Hallo Welt</h1>
+    </div>
+  );
+}
+
+export default App;
